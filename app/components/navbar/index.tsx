895b50f9b76e59ccc8e35b82685cab963ef00bb8
Malik Awais <malikawais371585@gmail.com>
2025-08-22 22:11:25 +0500

    fiexes

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from "react";
 import { Menu, X, Home, User, Mail, Briefcase } from "lucide-react";
 import Image from "next/image";
@@ -27,7 +28,7 @@ const Navbar = () => {
     { name: "Home", icon: Home, href: "/" },
     { name: "About", icon: User, href: "#about" },
     { name: "Contact", icon: Mail, href: "/contact" },
-    { name: "Portfolio", icon: Briefcase, href: "#portfolio" },
+    { name: "Portfolio", icon: Briefcase, href: "/portfolio" },
   ];
 
   return (
