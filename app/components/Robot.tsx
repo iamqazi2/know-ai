fc27925da30d52149384be8cfe57db520d513a0e
stixordev <adil.gillani@stixor.com>
2025-09-26 00:49:41 +0500

    robotSize

--- app/components/Robot.tsx
+++ app/components/Robot.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const Robot = () => {
     return (
-        <div className='w-full h-[400px] max-w-[800px] mx-auto relative z-100 top-14'>
+        <div className='w-full h-[345px] max-w-[800px] mx-auto relative z-100 top-18'>
             <Spline
                 scene="https://prod.spline.design/Kq9JyNofPlZVvMgR/scene.splinecode" 
             />
